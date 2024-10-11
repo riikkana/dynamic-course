b676e76bfecd0c700b6a94323cd99155c489b9d9
riikkana <naamanka.riikka@gmail.com>
2024-10-11 09:12:43 +0300

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Product from './components/Product'
+import OrderInfo from './components/OrderInfo'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <Product />
+        <OrderInfo />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/OrderInfo.jsx
@@ -0,0 +1,12 @@
+
+
+const OrderInfo = () => {
+    
+    return (
+        <div>
+            <h1> This is orderinfo </h1>
+        </div>
+    );
+}
+
+export default OrderInfo;
